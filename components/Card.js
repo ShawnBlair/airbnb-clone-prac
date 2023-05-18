f50e43c651dc232786a0295f4629b68470c4f1c2
ShawnBlair <shawnmwa98@gmail.com>
2023-05-18 18:03:00 +0300

    Creation of an airbnb clone webpage snippet

---
+++ components/Card.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Card(){
+    return(
+        <div className="card">
+        <img src="./images/katie-zaferes.png" className="katie"/>
+        <div className="strln"><img src="./images/star.png" className="rdstr"/>
+        <spam className="nord">5.0<spam className="grey">(6) •USA</spam></spam></div>
+        <p>Life Lessons with Katie Zaferes</p>
+        <p><spam className="doh">From $136</spam> / person</p>
+        </div>
+    )
+}
---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero(){
+    return(
+        <div>
+        <img src="./images/photo-grid.png" className="pgi"/>
+        <p className="oe">Online Experiences</p>
+        <p className="join">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
+        </div>
+    )
+}
