7a10e7b6ab1b52e92c1432f28a61b3e2438ee3ea
ShawnBlair <shawnmwa98@gmail.com>
2023-09-16 20:49:28 +0300

    m

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ import Card from "./components/Card"
 
 export default function App(){
     return(
-        <>
+        <div>
         <Navbar />
         <Hero />
         <Card />
-        </>
+        </div>
     )
 }
